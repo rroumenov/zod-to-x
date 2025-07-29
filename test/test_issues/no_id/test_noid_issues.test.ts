c90d90dd284ebd332d62378bc52f03cf9d86148b
Radoslav Roumenov Krastev <r.roumenovk@gmail.com>
2025-07-29 10:13:55 +0200

    Imports declaration fix

--- test/test_issues/no_id/test_noid_issues.test.ts
+++ test/test_issues/no_id/test_noid_issues.test.ts
@@ -5,8 +5,10 @@ extendZod(z);
 import { describe } from "vitest";
 import { runCase1Suite } from "./1/case_1.test-suite";
 import { runCase2Suite } from "./2/case_2.test-suite";
+import { runCase3Suite } from "./3/case_3.test-suite";
 
 describe("Test issues - No id", () => {
     runCase1Suite();
     runCase2Suite();
+    runCase3Suite();
 });
